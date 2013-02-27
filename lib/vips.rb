a4a4d713a1b951db21227f07c9096bccc24d4fc6
Igor Davydov <iskiche@gmail.com>
2013-02-27 16:45:15 +0400

    Missing file in require

--- lib/vips.rb
+++ lib/vips.rb
@@ -1,7 +1,7 @@
 # Vips will print warnings to stdout unless this is set
 ENV['IM_WARNING'] = "0"
 
-require 'vips_ext'
+# require 'vips_ext'
 require 'vips/version'
 require 'vips/reader'
 require 'vips/writer'
